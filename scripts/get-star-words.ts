b05e414917753f51df2890f11a5edd1b6f870ea2
nus3 <hadayota33@gmail.com>
2021-12-30 17:53:49 +0900

    :sparkles: Add script to get star word data

---
+++ scripts/get-star-words.ts
@@ -0,0 +1,85 @@
+import puppeteer from "puppeteer-core";
+import { writeFileSync, mkdirSync, existsSync } from "fs";
+
+const { SCRAPING_URL } = process.env;
+
+type StarData = {
+  date: string;
+  name: string;
+  sign: string;
+  word: string;
+};
+
+const months = [
+  "january",
+  "february",
+  "march",
+  "april",
+  "may",
+  "june",
+  "july",
+  "august",
+  "september",
+  "october",
+  "november",
+  "december",
+];
+
+const arrayChunk = <T extends any[]>(array: T, size = 1) => {
+  return array.reduce(
+    (acc, _, index) =>
+      index % size ? acc : [...acc, array.slice(index, index + size)],
+    [] as T[][]
+  );
+};
+
+const main = async () => {
+  const browser = await puppeteer.launch({
+    channel: "chrome",
+  });
+  const page = await browser.newPage();
+
+  let starData: StarData[] = [];
+
+  for (const month of months) {
+    await page.goto(`${SCRAPING_URL}${month}.html`);
+    const data = await page.evaluate(() => {
+      const body = document.querySelector("tbody")?.textContent;
+      return body;
+    });
+    if (!data) {
+      await browser.close();
+      return;
+    }
+
+    const trimData = data
+      .replace(/ /g, "")
+      .split("\n")
+      .filter((d) => d !== "");
+
+    const chunk = arrayChunk(trimData, 4);
+    const d: StarData[] = chunk.map((c: string[]) => {
+      if (c.length !== 4) {
+        throw new Error("星言葉をとってきてるサイトの構造が変わったようだ!!");
+      }
+
+      return {
+        date: c[0].replace("月", "/").replace("日", ""),
+        name: c[1],
+        sign: c[2],
+        word: c[3],
+      };
+    });
+
+    starData = [...starData, ...d];
+  }
+
+  if (!existsSync("output")) {
+    mkdirSync("output");
+  }
+  writeFileSync("output/star-words.json", JSON.stringify(starData, null, 2));
+
+  await browser.close();
+};
+
+main();
