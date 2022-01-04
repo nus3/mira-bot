6c75c59089037a043f38146240f43ef1ec05d2bc
nus3 <hadayota33@gmail.com>
2022-01-04 11:58:11 +0900

    :+1: Add script to update star-words sheet

---
+++ src/addWords.ts
@@ -0,0 +1,21 @@
+function main() {
+  const sheetName = "star-words";
+  const sheet = SpreadsheetApp.getActiveSpreadsheet().getSheetByName(sheetName);
+
+  if (!sheet) return;
+
+  // 初期化処理
+  const lastRow = sheet.getLastRow();
+  if (lastRow !== 1) {
+    sheet.deleteRows(2, lastRow);
+  }
+
+  for (const starWord of Constants.STAR_DATA) {
+    sheet.appendRow([
+      starWord.date,
+      starWord.name,
+      starWord.sign,
+      starWord.word,
+    ]);
+  }
+}
